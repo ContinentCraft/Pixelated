2499dff1e3c2ab9e1000464799ba08aaabbd9e1f
ADMIN <admin@ADMINs-Mac-mini.local>
2022-10-03 12:42:09 -0500

    added rows

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
 // Your code here
+// const tr = document.getElementById('tr')
+// const newTd = document.createElement('td')
+// tr.appendChild(newTd)
+let currentColor = 'red'
+const table = document.getElementsByTagName('table')[0]
+
+function makeRow (){
+    const row = document.createElement('tr')
+    for (let i = 0; i < 20; i++){
+        const td = document.createElement('td')
+        row.appendChild(td)
+    }
+    table.appendChild(row)
+}
+
+makeRow()
+makeRow()
+
+const button = document.getElementById('add-row')
+button.addEventListener('click', makeRow)
+
+table.addEventListener('click', colorize)
+function colorize(event){
+    const target = event.target
+    if (target.className.length){
+        target.className = ''
+    } else {
+        target.className = currentColor
+    }
+
+    
+}
+
+const select = document.getElementById('color')
+
+select.addEventListener('change', colorChange)
+function colorChange(event){
+    currentColor = event.target.value
+    
+}
